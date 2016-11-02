135379240ee11efd17da73f384b0440c146dfdcd
Gates-Gibson <agates.gibson@gmail.com>
2016-11-01 20:01:56 -0500

    Set up screens and created DB
    created ASP .NET MVC 5 application and created a local database for
    storing information.

---
+++ Pizza Application/Pizza Application/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace Pizza_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new IndexViewModel);
+        }
+
+        public ActionResult About()
+        {
+            PizzaOrderDatabaseEntities dbe = new PizzaOrderDatabaseEntities();
+            ViewBag.Message = "Orders";
+
+            return View(dbe.Orders.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public class IndexViewModel
+        {
+            public string Name { set; get; }
+            public string phoneNumber { set; get;}
+            public string dateTime { set; get; }
+            public string numOfPizzas { set; get; }
+        }
+    }
+}
